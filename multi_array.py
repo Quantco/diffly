1c0050f23b46118bbae8552719ae6d0d7546e514
Marius Merkle <marius.merkle@quantco.com>
2026-03-19 20:21:26 +0100

    remove multi-array

--- multi_array.py
+++
@@ -1,6 +0,0 @@
-# %%
-import polars as pl
-# %%
-df = pl.DataFrame({"a": [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]}, schema={"a": pl.Array(inner=pl.UInt8, shape=(2, 2))})
-# %%
-df.select(pl.col("a").arr.get(1))
