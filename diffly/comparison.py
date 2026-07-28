74bd6531e83645159257a5080a2d5c05c7ca8f1e
Moritz Potthoff <moritz.potthoff@quantco.com>
2026-07-28 20:39:01 +0200

    fix

--- diffly/comparison.py
+++ diffly/comparison.py
@@ -728,7 +728,7 @@ class DataFrameComparison:
             return False
 
         return (
-            pl.concat([left, right], how="horizontal", strict=True)
+            pl.concat([left, right], how="horizontal")
             .select(
                 condition_equal_rows(
                     columns=common_columns,
